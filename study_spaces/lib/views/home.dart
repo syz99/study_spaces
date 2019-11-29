5fe0c702a9046b0cc7637174430f813df41a5b14
js665 <js665@duke.edu>
2019-11-29 00:52:51 -0500

    added tabbar at bottom

---
+++ study_spaces/lib/views/home.dart
@@ -0,0 +1,45 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
+import 'package:study_spaces/views/spaces.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(items: [
+        BottomNavigationBarItem(
+          icon: Icon(CupertinoIcons.home),
+          title: Text('Spaces'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(CupertinoIcons.book),
+          title: Text('Profile'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(CupertinoIcons.search),
+          title: Text('Analytics'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(CupertinoIcons.settings),
+          title: Text('Settings'),
+        ),
+      ]),
+      tabBuilder: (context, index) {
+        if (index == 0) {
+          return SpacesList(title:"example text");
+        }
+//        else if (index == 1) {
+//          return FavoritesScreen();
+//        } else if (index == 2) {
+//          return SearchScreen();
+//        } else {
+//          return SettingsScreen();
+//        }
+      },
+    );
+  }
+}
