c06cd436815996e499779db5db41a1db02405690
js665 <js665@duke.edu>
2019-12-10 00:49:09 -0500

    refactored spaces_list to add button

--- study_spaces/lib/views/spaces_list.dart
+++ study_spaces/lib/views/spaces_list.dart
@@ -7,15 +7,11 @@ import 'package:study_spaces/data_models/app_state.dart';
 import 'package:study_spaces/data_models/space.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-
-
 class SpacesList extends StatelessWidget {
-
   Widget _generateSpaceRow(StudySpace space) {
     return Padding(
         padding: EdgeInsets.only(left: 16, right: 16, bottom: 24),
-        child: SpacesCard(space)
-    );
+        child: SpacesCard(space));
   }
 
   @override
@@ -23,49 +19,69 @@ class SpacesList extends StatelessWidget {
     return CupertinoTabView(
       builder: (context) {
         String dateString = DateFormat("MMMM y").format(DateTime.now());
+        return  DecoratedBox(
+                decoration: BoxDecoration(color: Color(0xffffffff)),
+                child: StreamBuilder(
+                    stream: Firestore.instance.collection('spaces').snapshots(),
+                    builder: (context, snapshot) {
+                      if (!snapshot.hasData) {
+                        return Text('Loading');
+                      } else {
+                        return ListView.builder(
+                          itemCount: snapshot.data.documents.length,
+                          itemBuilder: (context, index) {
+                            if (index == 0) {
+                              return Padding(
+                                padding:
+                                    const EdgeInsets.fromLTRB(16, 24, 16, 16),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(dateString.toUpperCase()),
+                                    new SizedBox(
+                                      height: 40,
+                                      child: new Row(
+                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                          children: <Widget>[
+                                            Text('Study Spaces'),
+                                            SizedBox(
+                                                height: 40,
+                                                width: 100,
+                                                child: Container(
+                                                    child: FlatButton(
+                                                      shape: new RoundedRectangleBorder(
+                                                          borderRadius: new BorderRadius.circular(18.0),
+                                                          side: BorderSide(color: Colors.red)),
+                                                      child: new Text('Add a new space'),
+                                                      textColor: Colors.red,
+                                                      onPressed: () {},))),
 
-
-        return DecoratedBox(
-            decoration: BoxDecoration(color: Color(0xffffffff)),
-            child: StreamBuilder(stream: Firestore.instance.collection('spaces').snapshots(), builder: (context, snapshot){
-              if(!snapshot.hasData){
-                return Text('Loading');
-              }
-              else{
-                return ListView.builder(
-                  itemCount: snapshot.data.documents.length,
-                  itemBuilder: (context, index){
-                    if (index == 0) {
-                      return Padding(
-                        padding: const EdgeInsets.fromLTRB(16, 24, 16, 16),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(dateString.toUpperCase()),
-                            Text('Study Spaces'),
-                          ],
-                        ),
-                      );
+                                          ]),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            } else if (index <=
+                                snapshot.data.documents.length) {
+                              DocumentSnapshot myspace =
+                                  snapshot.data.documents[index];
+                              return _generateSpaceRow(
+                                  StudySpace.fromMap(myspace.data));
+                            } else {
+                              int relativeIndex =
+                                  index - (snapshot.data.documents.length + 2);
+                              DocumentSnapshot myspace =
+                                  snapshot.data.documents[relativeIndex];
+                              return _generateSpaceRow(
+                                  StudySpace.fromMap(myspace.data));
+                            }
+                          },
+                        );
+                      }
                     }
-                   else if (index <= snapshot.data.documents.length) {
-                      DocumentSnapshot myspace = snapshot.data.documents[index];
-                      return _generateSpaceRow(StudySpace.fromMap(myspace.data));
-                    }
-                   else{
-                      int relativeIndex = index - (snapshot.data.documents.length + 2);
-                      DocumentSnapshot myspace = snapshot.data.documents[relativeIndex];
-                      return _generateSpaceRow(StudySpace.fromMap(myspace.data));
-                    }
-
-                  },
-                );
-
-              }
-
-            }
-            ));
+                    )
+        );
       },
     );
   }
-
 }
