bddc1bc2366f2a360095faa3fc6735e43c484207
Samuel Zhang <samzyao@gmail.com>
2019-12-10 00:31:16 -0500

    stash

--- study_spaces/lib/main.dart
+++ study_spaces/lib/main.dart
@@ -4,43 +4,47 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'package:study_spaces/data_models/app_state.dart';
 import 'package:study_spaces/util/authentication.dart';
+import 'package:study_spaces/views/login.dart';
 import 'package:study_spaces/views/root.dart';
 
-class MyApp extends StatelessWidget{
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Study Spaces',
-      debugShowCheckedModeBanner: false,
-//      theme: ThemeData(
-//        primarySwatch: Colors.blue,
-//      ),
-      color: Color(0xffd0d0d0),
-      home: RootScreen(auth: new Auth()),
-    );
-  }
-}
-
-void main() => runApp(MyApp());
-
-//void main() {
-//  SystemChrome.setPreferredOrientations([
-//    DeviceOrientation.portraitUp,
-//    DeviceOrientation.portraitDown,
-//  ]);
-//  final model = AppState();
-//  runApp(
-//    ScopedModel<AppState>(
-//      model: model,
-//      child: CupertinoApp(
-//        debugShowCheckedModeBanner: false,
-//        color: Color(0xffd0d0d0),
-//        home: LoginScreen(),
-////        home: LoginSignupScreen(),
-//      ),
-//    ),
-//  );
+//class MyApp extends StatelessWidget{
+//  // This widget is the root of your application.
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      title: 'Study Spaces',
+//      debugShowCheckedModeBanner: false,
+////      theme: ThemeData(
+////        primarySwatch: Colors.blue,
+////      ),
+//      color: Color(0xffd0d0d0),
+//      home: RootScreen(auth: new Auth()),
+//    );
+//  }
 //}
+//
+//void main() => runApp(MyApp());
+
+void main() {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  final model = AppState();
+  runApp(
+    ScopedModel<AppState>(
+      model: model,
+      child: CupertinoApp(
+        debugShowCheckedModeBanner: false,
+        color: Color(0xffd0d0d0),
+        home: LoginScreen(),
+//        home: LoginSignupScreen(),
+      ),
+    ),
+  );
+}
 
